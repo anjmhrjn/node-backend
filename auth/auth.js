e1410888a3bdc32ba353a7bbb6fe883b35bf927d
anjmhrjn <anjmhrjn1@gmail.com>
2022-02-05 00:28:31 +0545

    booking update endpoint created

--- auth/auth.js
+++ auth/auth.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 const item = require('../models/itemModel');
 const table = require('../models/tableModel');
 const user = require('../models/userModel');
+const booking = require('../models/bookingModel');
 
 module.exports.verifyUser = function(req, res, next) {
     try {
@@ -199,6 +200,32 @@ module.exports.verifyItem = function(req, res, next) {
             item.findOne({_id: req.params.itemId}).then(function(itemData) {
                 if (itemData.itemOf.toString() === udata.userId) {
                     req.itemInfo = itemData;
+                    next();
+                } else {
+                    res.json({message: "Method not allowed"})
+                }
+            })
+            .catch(function(e) {
+                res.json({error: e})
+            })
+        } else {
+            res.json({error: "Authentication details not provided"})
+        }
+    }
+    catch(e) {
+        res.json({error: e})
+    }
+}
+
+module.exports.verifyBooking = function(req, res, next) {
+    try {
+        const authorization_val = req.headers.authorization
+        if (authorization_val !== undefined) {
+            const token = req.headers.authorization.split(" ")[1];
+            const udata = jwt.verify(token, "restrobooking");
+            booking.findOne({_id: req.params.bid}).then(function(bookingData) {
+                if (bookingData.user.toString() === udata.userId) {
+                    req.bookingInfo = bookingData;
                     next();
                 } else {
                     res.json({message: "Method not allowed"})
--- router/bookingRoute.js
+++ router/bookingRoute.js
@@ -72,4 +72,61 @@ router.post("/book/table", auth.verifyCustomer, function(req, res) {
     })
 })
 
+router.put("/update/booking/:bid", auth.verifyBooking, function(req, res) {
+    const bookingId = req.params.bid
+    const requested_start_time = new Date(req.body.start_time)
+    const requested_end_time = new Date(req.body.end_time)
+    booking.find({"user": req.body.user, "requested_for": req.body.requested_for})
+    .then(function(result) {
+        let has_booking = false
+        result.map(user_booking => {
+            const start_time = user_booking.start_time
+            const end_time = user_booking.end_time
+            if (user_booking._id.toString() !== bookingId) {
+                if (checkTime(start_time, end_time, requested_start_time)) {
+                    has_booking = true
+                }
+                if (checkTime(start_time, end_time, requested_end_time)) {
+                    has_booking = true
+                }
+            }
+        })
+        if (has_booking) {
+            res.json({message: "You already have a booking at that time.", success: false})
+        } else {
+            booking.find({"table": req.body.table, "requested_for": req.body.requested_for})
+            .then(function(result) {
+                result.map(table_booking => {
+                    const start_time = table_booking.start_time
+                    const end_time = table_booking.end_time
+                    if (table_booking._id.toString() !== bookingId) {
+                        if (checkTime(start_time, end_time, requested_start_time)) {
+                            has_booking = true
+                        }
+                        if (checkTime(start_time, end_time, requested_end_time)) {
+                            has_booking = true
+                        }
+                    }
+                })
+                if (has_booking) {
+                    res.json({message: "The table is already booked for that time.", success: false})
+                } else {
+                    booking.updateOne({_id: bookingId}, req.body)
+                    .then(function() {
+                        res.json({message: "Booking Updated", success: true});
+                    }).catch(function() {
+                        res.json({message: "Error in updating booking", success: false});
+                    })
+                }
+            })
+            .catch(function(e) {
+                console.log(e)
+            })
+        }
+    })
+    .catch(function(e) {
+        console.log(e)
+    })
+})
+
 module.exports = router
